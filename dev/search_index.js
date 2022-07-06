54464fd7812edec2ff3c5a469f397e2d885dd1fb
Documenter.jl <documenter@juliadocs.github.io>
2022-07-06 16:13:52 +0000

    build based on a71a123

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ONSAS","category":"page"},{"location":"#ONSAS","page":"Home","title":"ONSAS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ONSAS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ONSAS]","category":"page"}]
+}
