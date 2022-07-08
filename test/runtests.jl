336886d752189392ade8f2c3275526780bd53bfe
jorgepz <jorgepz@fing.edu.uy>
2022-07-08 14:50:43 -0300

    fix error adding explicit call

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,17 @@
 using ONSAS
 using Test
 
-@testset "ONSAS.jl" begin
+using  LinearAlgebra
 
+
+@testset "ONSAS.jl" begin
     @testset "Test: linear von mises problem" begin
         include( joinpath( "..", "examples", "vonMisesTruss", "vonMisesTruss.jl" ) )
     
-        L0 = sqrt( d^2 + h^2 ) ;
+        L0 = sqrt( d^2 + h^2 )
     
-        cosalpha = d / L0 ;
-        sinalpha = h / L0 ;
+        cosalpha = d / L0
+        sinalpha = h / L0
     
         Ldef12 = norm( [ d+UG[7],h+UG[11]] - [0,0]  )
         Ldef23 = norm( [2d,0] - [ d+UG[7],h+UG[11]] )
@@ -24,6 +26,7 @@ using Test
         delta23Analy = F23Analy*L0 / (E*A) ;
     
         @test maximum( abs.( [ delta12Num-delta12Analy, delta12Num-delta12Analy ] ) ) < 1e-8
-      end
+    end
+
     
 end
