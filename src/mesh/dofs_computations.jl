fcfe2ccba91f2b2805b3ec529a7147cff0c686f5
joaquinv <joaquinv@fing.edu.uy>
2022-12-14 22:59:38 -0300

    change in ndoes2dofs

--- src/mesh/dofs_computations.jl
+++ src/mesh/dofs_computations.jl
@@ -2,22 +2,22 @@
 computeElemsByMEBI
 This function computes the vector of the element indexes for a given vector of MEBI parameter
 """
-function computeElemsByMEBI( MEBIVals, MEBIVec )
+function computeElemsByMEBI(MEBIVals, MEBIVec)
 
     # get the relevant BCs definitions
-    MEBIUnique = sort( unique( MEBIVals ) )     # get and sort them
-    MEBIUnique[1]==0 && popfirst!( MEBIUnique ) # and remove zeros
+    MEBIUnique = sort(unique(MEBIVals))     # get and sort them
+    MEBIUnique[1] == 0 && popfirst!(MEBIUnique) # and remove zeros
 
-    elements = [] 
+    elements = []
     # loop
     print(MEBIUnique)
     for MEBIval in MEBIUnique
-        print("\nMEBI:", MEBIval, "\n" )
+        print("\nMEBI:", MEBIval, "\n")
         # find the elements with the current MEBIval
-        aux = findall( x->x==MEBIval, MEBIVec )
-        
+        aux = findall(x -> x == MEBIval, MEBIVec)
+
         # if there are any, add them to the list
-        length(aux) > 0 && push!( elements, aux )
+        length(aux) > 0 && push!(elements, aux)
     end
     return elements
 end
@@ -26,24 +26,25 @@ end
 """
 nodes2dofs computes the vector of dofs for an input vector of nodes
 """
-function nodes2dofs( nodes , degreespernode )
-    n    = length(nodes);
-    dofs = zeros( Int, n*degreespernode ) ;
-    for i in (1:n);
-        print("\ni:",i,"\n")
-        print( (i-1)*degreespernode .+ Vector(1:degreespernode) )
-        print((degreespernode*(nodes[i]-1)).+Vector(1:degreespernode))
-        dofs[ (i-1)*degreespernode .+ Vector(1:degreespernode) ] = (degreespernode*(nodes[i]-1)).+Vector(1:degreespernode)  ;
+function nodes2dofs(nodes::Vector{Int}, ndofs::Int)
+    n = length(nodes)
+    gdl = Vector{Int}(undef, n * ndofs)
+    @inbounds for i in 1:n
+        α = (nodes[i] - 1) * ndofs
+        for j in 1:ndofs
+            β = (i - 1) * ndofs
+            gdl[β+j] = α + j
+        end
     end
-    return dofs;
+    return gdl
 end
 
 
 
-function compute_sparse_indexes( matrix, dofsElem )
+function compute_sparse_indexes(matrix, dofsElem)
 
-    num_cols = size( matrix, 2)
-    num_dofs = length( dofsElem )
+    num_cols = size(matrix, 2)
+    num_dofs = length(dofsElem)
 
     num_cols != num_cols && error("number of dofs must be equal to matrix cols")
 
@@ -52,10 +53,10 @@ function compute_sparse_indexes( matrix, dofsElem )
     vals_vector = Vector{Float64}()
 
     for i in (1:num_cols)
-        
-        append!(row_indexes, dofsElem )
-        append!(col_indexes, dofsElem[i]*ones(num_dofs) )
-        append!(vals_vector, matrix[:,i])
+
+        append!(row_indexes, dofsElem)
+        append!(col_indexes, dofsElem[i] * ones(num_dofs))
+        append!(vals_vector, matrix[:, i])
     end
 
     return row_indexes, col_indexes, vals_vector
