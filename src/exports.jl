ac55f0bc14ffe16d0c084bb5f8a487f682aa24b9
joaquinv <joaquinv@fing.edu.uy>
2022-12-16 21:12:10 -0300

    minor change in exports

--- src/exports.jl
+++ src/exports.jl
@@ -1,16 +1,21 @@
 
-export  Material,
-        CrossSection,
-        Geometry,
-        BoundaryCondition,
-        InitialCondition,
-        Mesh,
-        AnalysisSettings,
-        ONSAS_init,
-        ONSAS_solve,
-        ModelSolution,
-        ModelProperties,
-        Node,
-        Truss,
-        MeshB,
-        AbstractElement
+# Input Structs 
+export Material,
+    CrossSection,
+    Geometry,
+    BoundaryCondition,
+    InitialCondition,
+    Mesh,
+    AnalysisSettings
+
+# Model structs
+export ModelSolution,
+    ModelProperties
+
+# Functions
+export ONSAS_init,
+    ONSAS_solve,
+    Node,
+    Truss,
+    MeshB,
+    AbstractElement
