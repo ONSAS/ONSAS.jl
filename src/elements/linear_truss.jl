b7c0171513283714c05bc39ca3914bcdcf1fce98
joaquinv <joaquinv@fing.edu.uy>
2022-12-16 21:16:16 -0300

    comments in linear_truss

--- src/elements/linear_truss.jl
+++ src/elements/linear_truss.jl
@@ -7,30 +7,27 @@ function linear_truss(material, geometry, nodalCoords::Matrix, u::Vector)
   E = material.constitutive_params[1]
   A = geometry.area
 
-
+  # Element geometry
   diff = SVector{3}(nodalCoords[2, :]) - SVector{3}(nodalCoords[1, :])
-
-  length = sqrt(diff' * diff)
-
+  length = sqrt(diff' * diff) # Element length
   (c, s) = (diff[1], diff[3]) ./ length
-
+  # Rotation matrix
   Qloc2glo = @SMatrix [c -s 0 0
     s c 0 0
     0 0 c -s
     0 0 s c]
-
+  # Stiffness matrix in local system
   Kloc = E * A / length * @SMatrix [1 0 -1 0
     0 0 0 0
     -1 0 1 0
     0 0 0 0]
-
+  # Stiffness matrix in global system
   Kglo = Qloc2glo * Kloc * Qloc2glo'
-
-  fint = Kglo * u
-
-  force_vectors = [fint]
+  # Internal forces
+  fᵢₙₜ = Kglo * u
+  # Store
+  force_vectors = [fᵢₙₜ]
   tangent_matrices = [Kglo]
 
   return force_vectors, tangent_matrices
-
 end
