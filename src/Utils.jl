1a6f91a1b91cc89ab173bdc15d9d44ea49b02305
mvanzulli <mvanzulli@fing.edu.uy>
2023-01-27 10:51:40 -0300

    ONSAS.Utils #26

--- src/Utils.jl
+++ src/Utils.jl
@@ -4,17 +4,7 @@
 
 module Utils
 
-export ScalarWrapper
 export label, solve
-
-"Scalar mutable struct to avoid making mutable larger structs"
-mutable struct ScalarWrapper{T}
-    x::T
-end
-
-@inline Base.getindex(s::ScalarWrapper) = s.x
-@inline Base.setindex!(s::ScalarWrapper, v) = s.x = v
-Base.copy(s::ScalarWrapper{T}) where {T} = ScalarWrapper{T}(copy(s.x))
 
 "Empty function to extract the object label"
 function label end
