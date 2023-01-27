d65bba351cfe8a208a297385592d2e95f61b4e29
mvanzulli <mvanzulli@fing.edu.uy>
2023-01-27 17:29:25 -0300

    Moment and Forces boundary conditions #25

--- src/interface/BoundaryConditions.jl
+++ src/interface/BoundaryConditions.jl
@@ -13,6 +13,9 @@ using Reexport: @reexport
 
 export AbstractBoundaryCondition, dofs, label
 export DisplacementBoundaryCondition, FixedDisplacementBoundaryCondition, PinnedDisplacementBoundaryCondition
+export GlobalLoadBoundaryCondition, load_factor_function
+export MᵢLoadBoundaryCondition, MⱼLoadBoundaryCondition, MₖLoadBoundaryCondition
+export FᵢLoadBoundaryCondition, FⱼLoadBoundaryCondition, FₖLoadBoundaryCondition
 
 
 """ Abstract supertype for a boundary condition.
@@ -75,12 +78,6 @@ struct FixedDisplacementBoundaryCondition{D,V} <: AbstractBoundaryCondition
     end
 end
 
-# Composed accessors 
-#TODO:fixme
-# for m in methodswith(AbstractBoundaryCondition)
-#     print(m)
-#     @eval $m(fbc::FixedDisplacementBoundaryCondition) = $m(fbc.bc)
-# end
 dofs(fbc::FixedDisplacementBoundaryCondition) = dofs(fbc.bc)
 Base.values(fbc::FixedDisplacementBoundaryCondition) = values(fbc.bc)
 label(fbc::FixedDisplacementBoundaryCondition) = label(fbc.bc)
@@ -108,50 +105,190 @@ struct PinnedDisplacementBoundaryCondition{D,V} <: AbstractBoundaryCondition
     end
 end
 
-# Composed accessors 
-#TODO:fixme
-# for m in methodswith(AbstractBoundaryCondition)
-#     print(m)
-#     @eval $m(fbc::FixedDisplacementBoundaryCondition) = $m(fbc.bc)
-# end
 dofs(pbc::PinnedDisplacementBoundaryCondition) = dofs(pbc.bc)
 Base.values(pbc::PinnedDisplacementBoundaryCondition) = values(pbc.bc)
 label(pbc::PinnedDisplacementBoundaryCondition) = label(pbc.bc)
-
-#=
 
 #############################
 # Load Boundary Conditions #
 #############################
 
+const DEFAULT_LOAD_FACTOR_FUNC = (t) -> 1.0
+
 """ Load boundary condition imposed in local coordinates of the element.
 ### Fields:
-- `dofs`    -- Degrees of freedom where the boundary condition is imposed. 
-- `vals`    -- Values imposed. 
-- `label`   -- Boundary condition label.
+- `dofs`        -- Degrees of freedom where the boundary condition is imposed. 
+- `vals`        -- Values imposed. 
+- `load_factor` -- Function to compute at each time step the load factor.
+- `label`       -- Boundary condition label.
 """
-Base.@kwdef struct LocalLoadBoundaryCondition{D,V} <: AbstractBoundaryCondition
+Base.@kwdef struct LocalLoadBoundaryCondition{D,V,F} <: AbstractBoundaryCondition
     dofs::D
     values::V
-    load_time_factor::Function = (t) -> 1.0
+    load_factor::F = DEFAULT_LOAD_FACTOR_FUNC
     label::Symbol = DEFAULT_LABEL
 end
+
+"Returns the load factor function"
+load_factor_function(lbc::LocalLoadBoundaryCondition) = lbc.load_time_factor
 
 """ Load boundary condition imposed in global coordinates of the element.
 ### Fields:
-- `dofs`             -- Degrees of freedom where the boundary condition is imposed. 
-- `vals`             -- Values imposed. 
-- `label`            -- Boundary condition label.
-- `load_time_factor` -- Function to compute the time factor of the load.
+- `dofs`        -- Degrees of freedom where the boundary condition is imposed. 
+- `values`      -- Values imposed. 
+- `load_factor` -- Function to compute at each time step the load factor.
+- `label`       -- Boundary condition label.
 """
-Base.@kwdef struct GlobalLoadBoundaryCondition{D,V} <: AbstractBoundaryCondition
+Base.@kwdef struct GlobalLoadBoundaryCondition{D,V,F} <: AbstractBoundaryCondition
     dofs::D
     values::V
+    load_factor::F = DEFAULT_LOAD_FACTOR_FUNC
     label::Symbol = DEFAULT_LABEL
-    load_time_factor::Function = (t) -> 1.0
 end
 
-""" User Load boundary condition imposed in global coordinates to entire structure.
+load_factor_function(gbc::GlobalLoadBoundaryCondition) = gbc.load_factor
+
+""" Mᵢ load boundary condition struct:
+
+This is a particular instance of the struct `GlobalLoadBoundaryCondition`
+    considering a positive moment along the `i` axis.
+### Fields:
+- `bc`   -- Mᵢ Load boundary condition. 
+"""
+struct MᵢLoadBoundaryCondition{F,V} <: AbstractBoundaryCondition
+    bc::GlobalLoadBoundaryCondition
+    function MᵢLoadBoundaryCondition(
+        v::V, load_factor::F=DEFAULT_LOAD_FACTOR_FUNC; label=DEFAULT_LABEL
+    ) where {F,V}
+        m_dofs = [:Mᵢ]
+        lbc = GlobalLoadBoundaryCondition(m_dofs, v, load_factor, label)
+        return new{F,V}(lbc)
+    end
+end
+
+dofs(Mᵢbc::MᵢLoadBoundaryCondition) = dofs(Mᵢbc.bc)
+Base.values(Mᵢbc::MᵢLoadBoundaryCondition) = values(Mᵢbc.bc)
+label(Mᵢbc::MᵢLoadBoundaryCondition) = label(Mᵢbc.bc)
+load_factor_function(Mᵢbc::MᵢLoadBoundaryCondition) = load_factor_function(Mᵢbc.bc)
+
+""" Mⱼ load boundary condition struct:
+
+This is a particular instance of the struct `GlobalLoadBoundaryCondition`
+    considering a positive moment along the `j` axis.
+### Fields:
+- `bc`   -- Mⱼ Load boundary condition. 
+"""
+struct MⱼLoadBoundaryCondition{F,V} <: AbstractBoundaryCondition
+    bc::GlobalLoadBoundaryCondition
+    function MⱼLoadBoundaryCondition(
+        v::V, load_factor::F=DEFAULT_LOAD_FACTOR_FUNC; label=DEFAULT_LABEL
+    ) where {F,V}
+        m_dofs = [:Mⱼ]
+        lbc = GlobalLoadBoundaryCondition(m_dofs, v, load_factor, label)
+        return new{F,V}(lbc)
+    end
+end
+
+dofs(Mⱼbc::MⱼLoadBoundaryCondition) = dofs(Mⱼbc.bc)
+Base.values(Mⱼbc::MⱼLoadBoundaryCondition) = values(Mⱼbc.bc)
+label(Mⱼbc::MⱼLoadBoundaryCondition) = label(Mⱼbc.bc)
+load_factor_function(Mⱼbc::MⱼLoadBoundaryCondition) = load_factor_function(Mⱼbc.bc)
+
+""" Mₖ load boundary condition struct:
+
+This is a particular instance of the struct `GlobalLoadBoundaryCondition`
+    considering a positive moment along the `j` axis.
+### Fields:
+- `bc`   -- Mₖ Load boundary condition. 
+"""
+struct MₖLoadBoundaryCondition{F,V} <: AbstractBoundaryCondition
+    bc::GlobalLoadBoundaryCondition
+    function MₖLoadBoundaryCondition(
+        v::V, load_factor::F=DEFAULT_LOAD_FACTOR_FUNC; label=DEFAULT_LABEL
+    ) where {F,V}
+        m_dofs = [:Mₖ]
+        lbc = GlobalLoadBoundaryCondition(m_dofs, v, load_factor, label)
+        return new{F,V}(lbc)
+    end
+end
+
+dofs(Mₖbc::MₖLoadBoundaryCondition) = dofs(Mₖbc.bc)
+Base.values(Mₖbc::MₖLoadBoundaryCondition) = values(Mₖbc.bc)
+label(Mₖbc::MₖLoadBoundaryCondition) = label(Mₖbc.bc)
+load_factor_function(Mₖbc::MₖLoadBoundaryCondition) = load_factor_function(Mₖbc.bc)
+
+""" Fᵢ load boundary condition struct:
+
+This is a particular instance of the struct `GlobalLoadBoundaryCondition`
+    considering a positive force along the `i` axis.
+### Fields:
+- `bc`  -- Fᵢ Load boundary condition. 
+"""
+struct FᵢLoadBoundaryCondition{F,V} <: AbstractBoundaryCondition
+    bc::GlobalLoadBoundaryCondition
+    function FᵢLoadBoundaryCondition(
+        v::V, load_factor::F=DEFAULT_LOAD_FACTOR_FUNC; label=DEFAULT_LABEL
+    ) where {F,V}
+        m_dofs = [:Fᵢ]
+        lbc = GlobalLoadBoundaryCondition(m_dofs, v, load_factor, label)
+        return new{F,V}(lbc)
+    end
+end
+
+dofs(Fᵢbc::FᵢLoadBoundaryCondition) = dofs(Fᵢbc.bc)
+Base.values(Fᵢbc::FᵢLoadBoundaryCondition) = values(Fᵢbc.bc)
+label(Fᵢbc::FᵢLoadBoundaryCondition) = label(Fᵢbc.bc)
+load_factor_function(Fᵢbc::FᵢLoadBoundaryCondition) = load_factor_function(Fᵢbc.bc)
+
+
+""" Fⱼ load boundary condition struct:
+
+This is a particular instance of the struct `GlobalLoadBoundaryCondition`
+    considering a positive force along the `i` axis.
+### Fields:
+- `bc`  -- Fⱼ Load boundary condition. 
+"""
+struct FⱼLoadBoundaryCondition{F,V} <: AbstractBoundaryCondition
+    bc::GlobalLoadBoundaryCondition
+    function FⱼLoadBoundaryCondition(
+        v::V, load_factor::F=DEFAULT_LOAD_FACTOR_FUNC; label=DEFAULT_LABEL
+    ) where {F,V}
+        m_dofs = [:Fⱼ]
+        lbc = GlobalLoadBoundaryCondition(m_dofs, v, load_factor, label)
+        return new{F,V}(lbc)
+    end
+end
+
+dofs(Fⱼbc::FⱼLoadBoundaryCondition) = dofs(Fⱼbc.bc)
+Base.values(Fⱼbc::FⱼLoadBoundaryCondition) = values(Fⱼbc.bc)
+label(Fⱼbc::FⱼLoadBoundaryCondition) = label(Fⱼbc.bc)
+load_factor_function(Fⱼbc::FⱼLoadBoundaryCondition) = load_factor_function(Fⱼbc.bc)
+
+
+""" Fₖ load boundary condition struct:
+
+This is a particular instance of the struct `GlobalLoadBoundaryCondition`
+    considering a positive force along the `i` axis.
+### Fields:
+- `bc`  -- Fₖ Load boundary condition. 
+"""
+struct FₖLoadBoundaryCondition{F,V} <: AbstractBoundaryCondition
+    bc::GlobalLoadBoundaryCondition
+    function FₖLoadBoundaryCondition(
+        v::V, load_factor::F=DEFAULT_LOAD_FACTOR_FUNC; label=DEFAULT_LABEL
+    ) where {F,V}
+        m_dofs = [:Fₖ]
+        lbc = GlobalLoadBoundaryCondition(m_dofs, v, load_factor, label)
+        return new{F,V}(lbc)
+    end
+end
+
+dofs(Fₖbc::FₖLoadBoundaryCondition) = dofs(Fₖbc.bc)
+Base.values(Fₖbc::FₖLoadBoundaryCondition) = values(Fₖbc.bc)
+label(Fₖbc::FₖLoadBoundaryCondition) = label(Fₖbc.bc)
+load_factor_function(Fₖbc::FₖLoadBoundaryCondition) = load_factor_function(Fₖbc.bc)
+
+""" User Load boundary condition imposed in global coordinates to the entire structure.
 ### Fields:
 - `f`    -- User load function (the output of this function must be a vector with length equals to the number of dofs). 
 """
@@ -159,12 +296,18 @@ struct UserLoadsBoundaryCondition <: AbstractBoundaryCondition
     f::Function
 end
 
+""" Spring boundary condition imposed in global coordinates of the element.
+### Fields:
+- `dofs`             -- Degrees of freedom where the spring force is imposed. 
+- `spring_constants` -- Spring constant `k = f/u` for each dof. 
+"""
 
-struct SpringsBoundaryCondition <: AbstractDofs
-    # to do
+struct SpringsBoundaryCondition{D,V} <: AbstractBoundaryCondition
+    dofs::D
+    spring_constants::V
 end
 
-=#
+
 end # module
 
 
--- test/boundary_conditions.jl
+++ test/boundary_conditions.jl
@@ -3,9 +3,13 @@
 #############################
 using Test: @testset, @test
 using ONSAS.BoundaryConditions
-using ONSAS.BoundaryConditions: DEFAULT_LABEL
+using ONSAS.BoundaryConditions: DEFAULT_LABEL, DEFAULT_LOAD_FACTOR_FUNC
 
 @testset "ONSAS.BoundaryConditions.DisplacementBoundaryCondition" begin
+
+    ###############
+    # Constructors
+    ###############
 
     # Generic labeled boundary condition
     generic_fixed_dofs = [:uᵢ, :uⱼ, :θⱼ, :uₖ, :θₖ]
@@ -37,6 +41,86 @@ using ONSAS.BoundaryConditions: DEFAULT_LABEL
     @test values(pinned_bc) == zeros(length(dofs(pinned_bc)))
     @test label(pinned_bc) == label_pinned
 
+end
 
+
+@testset "ONSAS.BoundaryConditions.LoadBoundaryCondition" begin
+
+    ###############
+    # Constructors
+    ###############
+
+    # Generic labeled global load boundary condition
+    generic_load_dofs = [:Mᵢ, :Fⱼ, :Mₖ]
+    generic_values = rand(3)
+    generic_bc_label = :bc_generic
+    load_fact_generic(t) = [sin(t), t, t^2]
+    generic_bc = GlobalLoadBoundaryCondition(
+        generic_load_dofs, generic_values, load_fact_generic, generic_bc_label
+    )
+
+    # Accessors 
+    @test dofs(generic_bc) == generic_load_dofs
+    @test values(generic_bc) == generic_values
+    @test load_factor_function(generic_bc) == load_fact_generic
+    @test label(generic_bc) == generic_bc_label
+
+    # Generic labeled global load boundary condition without load factor function defined
+    generic_bc = GlobalLoadBoundaryCondition(dofs=generic_load_dofs, values=generic_values)
+    @test load_factor_function(generic_bc) == DEFAULT_LOAD_FACTOR_FUNC
+    @test label(generic_bc) == DEFAULT_LABEL
+
+
+    # Load boundary condition: Moment along `x` axis 
+    Mᵢ_val = rand(Float32, 1)
+    Mᵢ_load_factor_func = (t) -> sin(t)
+    Mᵢ_bc = MᵢLoadBoundaryCondition(Mᵢ_val..., Mᵢ_load_factor_func)
+    @test dofs(Mᵢ_bc) == [:Mᵢ]
+    @test values(Mᵢ_bc) == Mᵢ_val[1]
+    @test label(Mᵢ_bc) == DEFAULT_LABEL
+    @test load_factor_function(Mᵢ_bc) == Mᵢ_load_factor_func
+
+    # Load boundary condition: Moment along `y` axis 
+    Mⱼ_val = rand(Int, 1)
+    Mⱼ_bc = MⱼLoadBoundaryCondition(Mⱼ_val...)
+    @test dofs(Mⱼ_bc) == [:Mⱼ]
+    @test values(Mⱼ_bc) == Mⱼ_val[1]
+    @test label(Mⱼ_bc) == DEFAULT_LABEL
+    @test load_factor_function(Mⱼ_bc) == DEFAULT_LOAD_FACTOR_FUNC
+
+    # Load boundary condition: Moment along `z` axis 
+    Mₖ_val = rand(Int, 1)
+    Mₖ_label = :my_moment_bc
+    Mₖ_bc = MₖLoadBoundaryCondition(Mₖ_val..., label=Mₖ_label)
+    @test dofs(Mₖ_bc) == [:Mₖ]
+    @test values(Mₖ_bc) == Mₖ_val[1]
+    @test label(Mₖ_bc) == Mₖ_label
+    @test load_factor_function(Mₖ_bc) == DEFAULT_LOAD_FACTOR_FUNC
+
+    # Load boundary condition: force along `x` axis 
+    Fᵢ_val = rand(Float32, 1)
+    Fᵢ_load_factor_func = (t) -> sin(t)
+    Fᵢ_bc = FᵢLoadBoundaryCondition(Fᵢ_val..., Fᵢ_load_factor_func)
+    @test dofs(Fᵢ_bc) == [:Fᵢ]
+    @test values(Fᵢ_bc) == Fᵢ_val[1]
+    @test label(Fᵢ_bc) == DEFAULT_LABEL
+    @test load_factor_function(Fᵢ_bc) == Fᵢ_load_factor_func
+
+    # Load boundary condition: force along `y` axis 
+    Fⱼ_val = rand(Int, 1)
+    Fⱼ_bc = FⱼLoadBoundaryCondition(Fⱼ_val...)
+    @test dofs(Fⱼ_bc) == [:Fⱼ]
+    @test values(Fⱼ_bc) == Fⱼ_val[1]
+    @test label(Fⱼ_bc) == DEFAULT_LABEL
+    @test load_factor_function(Fⱼ_bc) == DEFAULT_LOAD_FACTOR_FUNC
+
+    # Load boundary condition: force along `z` axis 
+    Fₖ_val = rand(Int, 1)
+    Fₖ_label = :Fy_force_bc
+    Fₖ_bc = FₖLoadBoundaryCondition(Fₖ_val..., label=Fₖ_label)
+    @test dofs(Fₖ_bc) == [:Fₖ]
+    @test values(Fₖ_bc) == Fₖ_val[1]
+    @test label(Fₖ_bc) == Fₖ_label
+    @test load_factor_function(Fₖ_bc) == DEFAULT_LOAD_FACTOR_FUNC
 
 end
