419f692c922eebdb8c97652806552e339cfe9d79
mvanzulli <mvanzulli@fing.edu.uy>
2023-02-27 12:06:53 -0300

    uniaxial extension test (#41)

--- examples/uniaxialExtension/uniaxialExtension.jl
+++ examples/uniaxialExtension/uniaxialExtension.jl
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------- 
-# Unaxial Extension ExampleExercise 4 from section 6.5 in (Holzapfel,2000).
+# Uniaxial Extension ExampleExercise 4 from section 6.5 in (Holzapfel,2000).
 # --------------------------------------------------------------------------
 using ONSAS.StaticAnalyses
 using Test: @test
@@ -7,17 +7,14 @@ using Test: @test
 # scalar parameters
 E = 1.0 # Young modulus in Pa
 ν = 0.3  # Poisson's ratio
-p = 2   # Tension load in Pa
+p = 3   # Tension load in Pa
 Lᵢ = 2.0   # Dimension in x of the box in m 
 Lⱼ = 1.0   # Dimension in y of the box in m
 Lₖ = 1.0   # Dimension in z of the box in m
+const RTOL = 1e-4               # Relative tolerance for tests
 # -------------------------------
-# Materials
-# -------------------------------
-steel = SVK(E, ν, "steel")
-# ------
 # Mesh
-# ------
+#--------------------------------
 n₁ = Node(0.0, 0.0, 0.0)
 n₂ = Node(0.0, 0.0, Lₖ)
 n₃ = Node(0.0, Lⱼ, Lₖ)
@@ -48,4 +45,55 @@ t₅ = Tetrahedron(n₄, n₆, n₅, n₈, "tetra_5")
 t₆ = Tetrahedron(n₄, n₇, n₆, n₈, "tetra_6")
 vec_elems = [t₁, t₂, t₃, t₄, t₅, t₆]
 push!(s_mesh, vec_elems)
+# -------------------------------
+# Dofs
+#--------------------------------
+dof_dim = 3
+add!(s_mesh, :u, dof_dim)
+# -------------------------------
+# Materials
+# -------------------------------
+svk = SVK(E, ν, "steel")
+mat_dict = dictionary([svk => [t₁, t₂, t₃, t₄, t₅, t₆]])
+s_materials = StructuralMaterials(mat_dict)
+# -------------------------------
+# Boundary conditions
+# -------------------------------
+# Fixed dofs
+bc₁ = FixedDofBoundaryCondition([:u], [1], "fixed_uₓ")
+bc₂ = FixedDofBoundaryCondition([:u], [2], "fixed_uⱼ")
+bc₃ = FixedDofBoundaryCondition([:u], [3], "fixed_uₖ")
+# Load
+bc₄ = GlobalLoadBoundaryCondition([:u], t -> [p * t, 0, 0], "tension")
+# Assign this to faces 
+face_bc = dictionary([bc₁ => [f₃, f₄], bc₂ => [f₅, f₆], bc₃ => [f₇, f₈], bc₄ => [f₁, f₂]])
+# Crete boundary conditions struct
+s_boundary_conditions = StructuralBoundaryConditions(face_bcs=face_bc)
+# -------------------------------
+# Structure
+# -------------------------------
+s = Structure(s_mesh, s_materials, s_boundary_conditions)
+# -------------------------------
+# Structural Analysis
+# -------------------------------
+# Final load factor
+NSTEPS = 8
+sa = StaticAnalysis(s, NSTEPS=NSTEPS)
+# -------------------------------
+# Algorithm
+# -------------------------------
+tol_f = 1e-8;
+tol_u = 1e-8;
+max_iter = 30;
+tols = ConvergenceSettings(tol_u, tol_f, max_iter)
+nr = NewtonRaphson(tols)
+# -------------------------------
+# Numerical solution
+# -------------------------------
+states_sol = solve(sa, nr)
+numerical_uₖ = getindex.(displacements.(states_sol), index(Dof(19)))
 
+
+u_ONSAS = [0.5251, 0.8623, 1.1243, 1.3434, 1.5340, 1.7040, 1.8582, 2.0000]
+
+@test numerical_uₖ ≈ u_ONSAS rtol = RTOL
