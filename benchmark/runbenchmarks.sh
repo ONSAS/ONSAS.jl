c1fc63199ef6a64146a33459bd0bcec6d26b4b5f
Marcelo Forets <marcelo@planting.space>
2023-04-23 17:24:38 -0300

    add 0.1 refinement factor in bench

--- benchmark/runbenchmarks.sh
+++ benchmark/runbenchmarks.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-julia -e 'import Pkg; Pkg.activate(get(ENV, "ONSAS", @__DIR__)); using PkgBenchmark; results = benchmarkpkg("ONSAS", retune=true); export_markdown("results.md", results)'
+time julia -e 'import Pkg; Pkg.activate(get(ENV, "ONSAS", @__DIR__)); using PkgBenchmark; results = benchmarkpkg("ONSAS", retune=true); export_markdown("results.md", results)'
