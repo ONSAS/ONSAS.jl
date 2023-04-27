dd18e3072111cd08c7124ae584aac5464041087c
mvanzulli <mvanzulli@fing.edu.uy>
2023-04-27 21:07:26 +0200

    first draft cylinder into benchs (#245)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,90 +1,135 @@
 using BenchmarkTools, ONSAS, Suppressor
 
-include("bench_utils.jl")
+include("bench_utils.jl");
 
 # Parent BenchmarkGroup to contain our suite.
-SUITE = BenchmarkGroup()
+SUITE = BenchmarkGroup();
 
 # Number of evaluations for each example in the suite.
-evals = 3
+evals = 3;
 # Number of samples for each example in the suite.
-samples = 2
+samples = 2;
 
 # =======================================
 # Static analysis benchmarks
 # =======================================
 # Alg to solve static problems
-tols = ConvergenceSettings(; rel_U_tol=1e-8, rel_res_force_tol=1e-6, max_iter=20)
-alg = NewtonRaphson(tols)
+tols = ConvergenceSettings(; rel_U_tol=1e-8, rel_res_force_tol=1e-6, max_iter=20);
+alg = NewtonRaphson(tols);
 # Static analysis number of steps to reach the final load factor value
-NSTEPS = 8
+NSTEPS = 8;
 
-# ========================================
-# Uniaxial extension.
-# ========================================
-example_name = "uniaxial_extension"
-SUITE[example_name] = BenchmarkGroup()
-example_folder, bench_path = joinpath_example_folder(example_name)
-include(bench_path)
+# # ========================================
+# # Uniaxial extension.
+# # ========================================
+# example_name = "uniaxial_extension";
+# SUITE[example_name] = BenchmarkGroup();
+# example_folder, bench_path = joinpath_example_folder(example_name);
+# include(bench_path);
 
-# Refinement factors 
-ms_range = [0.5 0.4 0.3 0.2 0.1]
+# # Refinement factors 
+# ms_range = [0.5 0.4 0.3 0.2 0.1]
 
-for ms in ms_range
-    local structure
-    output = @capture_out begin
-        structure = uniaxial_extension_structure(; ms)
-    end
-    nnodes, nelems = capture_print(output)
-    problem = NonLinearStaticAnalysis(structure; NSTEPS=NSTEPS)
-    SUITE[example_name]["solve, ms = $ms, nelems = $nelems, nnodes = $nnodes"] = @benchmarkable solve!($problem,
-                                                                                                       $alg) evals = evals samples = samples
-end
+# for ms in ms_range
+#     local structure
+#     output = @capture_out begin
+#         structure = uniaxial_extension_structure(; ms)
+#     end
+#     nnodes, nelems = num_nodes(structure), num_elements(structure)
+#     problem = NonLinearStaticAnalysis(structure; NSTEPS=NSTEPS)
+#     SUITE[example_name]["solve, ms = $ms, nelems = $nelems, nnodes = $nnodes"] = @benchmarkable solve!($problem,
+#                                                                                                        $alg) evals = evals samples = samples
+# end
 
-# Remove all .msh files from the example_folder 
-delete_files(example_folder, ".msh")
+# # Remove all .msh files from the example_folder 
+# delete_files(example_folder, ".msh")
 
 # ===============================================================
 # Uniaxial compression.
 # ===============================================================
-example_name = "uniaxial_compression"
-SUITE[example_name] = BenchmarkGroup()
-example_folder, bench_path = joinpath_example_folder(example_name)
-include(bench_path)
+# example_name = "uniaxial_compression"
+# SUITE[example_name] = BenchmarkGroup()
+# example_folder, bench_path = joinpath_example_folder(example_name)
+# include(bench_path)
 
-for ms in ms_range
-    local structure
-    output = @capture_out begin
-        structure = uniaxial_compression_structure(; ms)
-    end
-    # Extract the number of elements as an integer.
-    nnodes, nelems = capture_print(output)
-    problem = NonLinearStaticAnalysis(structure; NSTEPS=NSTEPS)
-    SUITE[example_name]["solve, ms = $ms, nelems = $nelems, nnodes = $nnodes"] = @benchmarkable solve!($problem,
-                                                                                                       $alg) evals = evals samples = samples
-end
+# for ms in ms_range
+#     local structure
+#     output = @capture_out begin
+#         structure = uniaxial_compression_structure(; ms)
+#     end
+#     # Extract the number of elements as an integer.
+#     nnodes, nelems = num_nodes(structure), num_elements(structure)
+#     problem = NonLinearStaticAnalysis(structure; NSTEPS=NSTEPS)
+#     SUITE[example_name]["solve, ms = $ms, nelems = $nelems, nnodes = $nnodes"] = @benchmarkable solve!($problem,
+#                                                                                                        $alg) evals = evals samples = samples
+# end
 
-# Remove all .msh files from the example_folder 
-delete_files(example_folder, ".msh")
+# # Remove all .msh files from the example_folder 
+# delete_files(example_folder, ".msh")
 
 # ===============================================================
 # Linear extension.
 # ===============================================================
-example_name = "linear_extension"
-SUITE[example_name] = BenchmarkGroup()
-example_folder, bench_path = joinpath_example_folder(example_name)
-include(bench_path)
+# example_name = "linear_extension";
+# SUITE[example_name] = BenchmarkGroup();
+# example_folder, bench_path = joinpath_example_folder(example_name);
+# include(bench_path);
 
-for ms in ms_range
-    local structure
-    output = @capture_out begin
-        structure = linear_extension_structure(; ms)
+# for ms in ms_range
+#     local structure
+#     output = @capture_out begin
+#         structure = linear_extension_structure(; ms)
+#     end
+#     # Extract the number of elements as an integer.
+#     nnodes, nelems = num_nodes(structure), num_elements(structure)
+#     problem = LinearStaticAnalysis(structure; NSTEPS=NSTEPS)
+#     SUITE[example_name]["solve, ms = $ms, nelems = $nelems, nnodes = $nnodes"] = @benchmarkable solve!($problem) evals = evals samples = samples
+# end
+
+# # Remove all .msh files from the example_folder 
+# delete_files(example_folder, ".msh");
+
+# ===============================================================
+# Linear cylinder with internal pressure.
+# ===============================================================
+example_name = "linear_cylinder_internal_pressure";
+
+SUITE[example_name] = BenchmarkGroup();
+example_folder, bench_path = joinpath_example_folder(example_name);
+include(bench_path);
+
+ms_range = [0.5];
+n_points_each_axis_range = [10];
+
+ms = 0.5
+n_points_each_axis = 10
+
+structure = linear_cylinder_structure(; ms=ms);
+
+problem = LinearStaticAnalysis(structure; NSTEPS=NSTEPS);
+
+solution = solve!(problem);
+
+ph = point_eval_handler(structure; NPOINTS=n_points_each_axis);
+
+u_sol_ph = displacements(solution, ph);
+
+for n_points in n_points_each_axis_range
+    for ms in ms_range
+        local structure
+        structure = linear_cylinder_structure(; ms=$ms)
+
+        nnodes, nelems = num_nodes(structure), num_elements(structure)
+
+        problem = LinearStaticAnalysis(structure; NSTEPS=NSTEPS)
+
+        solution = @benchmarkable solve!($problem) evals = evals samples = samples
+
+        ph = point_eval_handler(structure; NPOINTS=n_points_each_axis)
+
+        eval_solution = displacements(solution, ph)
     end
-    # Extract the number of elements as an integer.
-    nnodes, nelems = capture_print(output)
-    problem = LinearStaticAnalysis(structure; NSTEPS=NSTEPS)
-    SUITE[example_name]["solve, ms = $ms, nelems = $nelems, nnodes = $nnodes"] = @benchmarkable solve!($problem) evals = evals samples = samples
-end
+end;
 
 # Remove all .msh files from the example_folder 
-delete_files(example_folder, ".msh")
+delete_files(example_folder, ".msh");
---
+++ benchmark/linear_cylinder_internal_pressure/bench_linear_cylinder_internal_pressure.jl
@@ -0,0 +1,111 @@
+# Include `create_mesh` function.
+include(joinpath(pkgdir(ONSAS), "examples", "cylinder_internal_pressure", "cylinder_mesh.jl"))
+
+"""
+Cylinder with internal pressure GMSH mesh and `IsotropicLinearElastic` material.
+
+`ms` is the refinement factor of the mesh.
+"""
+function linear_cylinder_structure(; ms::Real=0.5)
+
+    ## scalar parameters (dimensions in mm an MPa)
+    Lₖ = 30.0       # cylinder length in 𝐞ₖ mm
+    Rᵢ = 100.0      # inner radius in mm
+    Rₑ = 200.0      # outer radius in mm
+
+    E = 210.0       # Young modulus in MPa
+    ν = 0.3         # Poisson ratio
+
+    p = 25.0        # pressure in MPa
+    pressure(t) = p * t
+
+    # -------------------------------
+    # Physical entities labels
+    # -------------------------------
+    # material
+    mat_label = "mat"
+    # entities
+    node_label = "node"
+    faces_label = "triangle"
+    elements_label = "tetrahedron"
+    entities_labels = [node_label, faces_label, elements_label]
+    # boundary conditions
+    bc₁_label = "fixed-ui"
+    bc₂_label = "fixed-uj"
+    bc₃_label = "fixed-uk"
+    bc₄_label = "pressure"
+    bc_labels = [bc₁_label, bc₂_label, bc₃_label, bc₄_label]
+    # mesh labels
+    labels = [mat_label, entities_labels, bc_labels]
+    # -------------------------------
+    # Entities
+    # -------------------------------
+    # Entities types without assigned nodes, faces and elements
+    vfaces = [TriangularFace(faces_label)]
+    velems = [Tetrahedron(elements_label)]
+    entities = StructuralEntities(velems, vfaces)
+    # -------------------------------
+    # Mesh
+    # -------------------------------
+    filename = basename(tempname())
+    labels = [mat_label, entities_labels, bc_labels]
+    dir = joinpath(pkgdir(ONSAS), "benchmark", "linear_cylinder_internal_pressure")
+    filename = basename(tempname())
+    local msh_path
+    out = @capture_out begin
+        msh_path = create_cylinder_mesh(Rᵢ, Rₑ, Lₖ, labels, filename, ms, dir)
+    end
+    gmsh_println(out)
+    msh_mesh = MshFile(msh_path)
+    mesh = Mesh(msh_mesh, entities)
+    # Dofs
+    #--------------------------------
+    dof_dim = 3
+    dof_u_symbol = :u
+    apply!(mesh, dof_u_symbol, dof_dim)
+    # -------------------------------
+    # Boundary conditions
+    # -------------------------------
+    # Dirichlet boundary conditions 
+    bc₁ = FixedDofBoundaryCondition([:u], [1], bc₁_label)
+    bc₂ = FixedDofBoundaryCondition([:u], [2], bc₂_label)
+    bc₃ = FixedDofBoundaryCondition([:u], [3], bc₃_label)
+    # Neumann boundary conditions 
+    bc₄ = LocalPressureBoundaryCondition([:u], t -> pressure(t), bc₄_label)
+    boundary_conditions = StructuralBoundaryConditions(bc₁, bc₂, bc₃, bc₄)
+    # Assign boundary conditions to the ones defined in the mesh
+    apply!(boundary_conditions, mesh)
+    # -------------------------------
+    # Materials
+    # -------------------------------
+    material = IsotropicLinearElastic(E, ν, mat_label)
+    materials = StructuralMaterials(material)
+    apply!(materials, mesh)
+    # -------------------------------
+    # Structure
+    # -------------------------------
+
+    # Create mesh and retrieve the Structure
+    Structure(mesh, materials, boundary_conditions)
+end;
+
+"""
+Hyper rectangle starting at `O` pint and + [Lᵢ,Lⱼ,Lₖ] to evaluate the solution with `NPOINTS` in each axis.
+"""
+function point_eval_handler(structure::Structure;
+                            NPOINTS::Int=10)
+
+    ## scalar parameters (dimensions in mm an MPa)
+    Lₖ = 30.0                         # cylinder length in 𝐞ₖ mm
+    Rₑ = 200.0                       # outer radius in mm
+    Lᵢ = Lⱼ = 2.25Rₑ                # hyper rectangle origin in 𝐞ᵢ,𝐞ⱼ and  𝐞ₖ in mm    
+    O = (x=-Lᵢ / 2, y=-Lⱼ / 2, z=0.0)    # hyper rectangle origin in 𝐞ᵢ,𝐞ⱼ and  𝐞ₖ in mm
+
+    # Create an hyper rectangle Lᵢ x Lⱼ x Lₖ
+    x = LinRange(O.x, O.x + Lᵢ, NPOINTS)
+    y = LinRange(O.y, O.y + Lⱼ, NPOINTS)
+    z = LinRange(O.z, O.z + Lₖ, NPOINTS)
+
+    vec_points = [Point(p...) for p in vec(collect(Iterators.product(x, y, z)))]
+    PointEvalHandler(mesh(structure), vec_points)
+end;
