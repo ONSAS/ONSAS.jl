7f115826954d731a8ee436f83904acd59c9c8278
mvanzulli <mauricio.vanzulli@multiversecomputing.com>
2023-10-29 14:08:06 +0100

    benchmark bash and readme (#469)

--- benchmark/runbenchmarks.sh
+++ benchmark/runbenchmarks.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-time $JULIA_EXEC -e 'import Pkg; Pkg.activate(get(ENV, "ONSAS", @__DIR__)); using PkgBenchmark; results = benchmarkpkg("ONSAS", retune=true); export_markdown("results.md", results)'
+julia_command="julia"
+
+$julia_command -e 'import Pkg; Pkg.add("PkgBenchmark"); Pkg.add("BenchmarkTools")'
+time $julia_command -e 'import Pkg; Pkg.activate(get(ENV, "ONSAS", @__DIR__)); using PkgBenchmark; results = benchmarkpkg("ONSAS", retune=true); export_markdown("results.md", results)'
 
 find . -name "*.msh" -type f -delete
