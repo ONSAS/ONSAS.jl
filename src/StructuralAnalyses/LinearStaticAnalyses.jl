f3e6076b957cef89ff102ad022d9a4c172b55f50
mvanzulli <mauricio.vanzulli@multiversecomputing.com>
2023-10-29 12:44:03 +0100

    LinearSolve static analysis (#413)

--- src/StructuralAnalyses/LinearStaticAnalyses.jl
+++ src/StructuralAnalyses/LinearStaticAnalyses.jl
@@ -6,6 +6,7 @@ geometric linear static analysis.
 module LinearStaticAnalyses
 
 using IterativeSolvers
+using LinearSolve
 using Reexport
 
 using ..Utils
@@ -23,7 +24,7 @@ using ..Solutions
 
 # Since linear analysis do not iterate, the iteration state is:
 const LinearResidualsIterationStep = ResidualsIterationStep(nothing, nothing, nothing, nothing, 1,
-                                                            ΔU_and_ResidualForce_Criteria())
+	ΔU_and_ResidualForce_Criteria())
 
 export LinearStaticAnalysis
 
@@ -32,94 +33,103 @@ A linear analysis is a collection of parameters for defining the static analysis
 In the static analysis, the structure is analyzed at a given load factor (this variable is analog to time).
 As this analysis is linear the stiffness of the structure remains constant at each displacements iteration step.
 """
-mutable struct LinearStaticAnalysis{S<:AbstractStructure,R<:Real,LFV<:Vector{R}} <:
-               AbstractStaticAnalysis
-    "Structure to be analyzed."
-    const s::S
-    "Structural state."
-    const state::StaticState
-    "Load factors vector of the analysis."
-    const λᵥ::LFV
-    "Current load factor step."
-    current_step::Int64
+mutable struct LinearStaticAnalysis{S <: AbstractStructure, R <: Real, LFV <: Vector{R}} <:
+			   AbstractStaticAnalysis
+	"Structure to be analyzed."
+	const s::S
+	"Structural state."
+	const state::StaticState
+	"Load factors vector of the analysis."
+	const λᵥ::LFV
+	"Current load factor step."
+	current_step::Int64
 end
 
 "Constructor for linear analysis with load factors, optional initial step and initial state."
 function LinearStaticAnalysis(s::S, λᵥ::LFV;
-                              initial_state::StaticState=StaticState(s,
-                                                                     LinearResidualsIterationStep),
-                              initial_step::Int=1) where {S<:AbstractStructure,
-                                                          LFV<:Vector{<:Real}}
-    !(1 ≤ initial_step ≤ length(λᵥ)) &&
-        throw(ArgumentError("initial_step must be in [1, $(length(λᵥ))] but is: $initial_step."))
-    LinearStaticAnalysis(s, initial_state, λᵥ, initial_step)
+	initial_state::StaticState = StaticState(s,
+		LinearResidualsIterationStep),
+	initial_step::Int = 1) where {S <: AbstractStructure,
+	LFV <: Vector{<:Real}}
+	!(1 ≤ initial_step ≤ length(λᵥ)) &&
+		throw(ArgumentError("initial_step must be in [1, $(length(λᵥ))] but is: $initial_step."))
+	LinearStaticAnalysis(s, initial_state, λᵥ, initial_step)
 end
 
 "Constructor for linear analysis given a final time (or load factor) and the number of steps."
-function LinearStaticAnalysis(s::AbstractStructure, final_time::Real=1.0; NSTEPS=10,
-                              initial_state::StaticState=StaticState(s,
-                                                                     LinearResidualsIterationStep),
-                              initial_step::Int=1)
-    t₀ = final_time / NSTEPS
-    λᵥ = collect(LinRange(t₀, final_time, NSTEPS))
-    LinearStaticAnalysis(s, λᵥ; initial_state, initial_step)
+function LinearStaticAnalysis(s::AbstractStructure, final_time::Real = 1.0; NSTEPS = 10,
+	initial_state::StaticState = StaticState(s,
+		LinearResidualsIterationStep),
+	initial_step::Int = 1)
+	t₀ = final_time / NSTEPS
+	λᵥ = collect(LinRange(t₀, final_time, NSTEPS))
+	LinearStaticAnalysis(s, λᵥ; initial_state, initial_step)
 end
 
 function Base.show(io::IO, sa::LinearStaticAnalysis)
-    println("LinearStaticAnalysis for:")
-    println("• Current load factor $(sa.current_step))/$(length(load_factors(sa))).")
-    show(io, sa.s)
-    show(io, sa.state)
+	println("LinearStaticAnalysis for:")
+	println("• Current load factor $(sa.current_step))/$(length(load_factors(sa))).")
+	show(io, sa.s)
+	show(io, sa.state)
 end
 
 "Solves a linear analysis problem mutating the state."
-function _solve!(sa::LinearStaticAnalysis, ::DummySolver)
-    s = structure(sa)
+function _solve!(sa::LinearStaticAnalysis, ::LinearSolveDefault)
+	s = structure(sa)
 
-    # Initialize solution
-    solution = StatesSolution(sa, NewtonRaphson())
+	# Initialize solution
+	solution = StatesSolution(sa, NewtonRaphson())
 
-    # Load factors iteration.
-    while !is_done(sa)
+	# Load factors iteration.
+	while !is_done(sa)
 
-        # Compute external force
-        external_forces(current_state(sa)) .= 0
-        @debugtime "Assemble external forces" apply!(sa, load_bcs(boundary_conditions(s)))
+		# Compute external force
+		external_forces(current_state(sa)) .= 0
+		@debugtime "Assemble external forces" apply!(sa, load_bcs(boundary_conditions(s)))
 
-        # Assemble K
-        @debugtime "Assemble internal forces" assemble!(s, sa)
+		if sa.current_step == 1
+			# Assemble K
+			@debugtime "Assemble internal forces" assemble!(s, sa)
+		end
 
-        # Increment structure displacements U = ΔU
-        @debugtime "Step" step!(sa)
+		# Increment structure displacements U = ΔU
+		@debugtime "Step" step!(sa)
 
-        # Recompute σ and ε for the assembler
-        @debugtime "Assemble internal forces" assemble!(s, sa)
+		# Recompute σ and ε for the assembler
+		@debugtime "Update internal forces, stres and strains" assemble!(s, sa)
 
-        # Save current state
-        push!(solution, current_state(sa))
+		# Save current state
+		push!(solution, current_state(sa))
 
-        # Increments the time or load factor step
-        next!(sa)
-    end
+		# Increments the time or load factor step
+		next!(sa)
+	end
 
-    solution
+	solution
 end
 
 "Computes ΔU for solving the linear analysis."
 function step!(sa::LinearStaticAnalysis)
-    # Extract state info
-    state = current_state(sa)
-    free_dofs_idx = free_dofs(state)
+	# Extract state info
+	state = current_state(sa)
+	free_dofs_idx = free_dofs(state)
+	linear_system = state.linear_system
 
-    # Compute Δu
-    fₑₓₜ_red = view(external_forces(state), free_dofs_idx)
-    K = tangent_matrix(state)[free_dofs_idx, free_dofs_idx]
-    ΔU = Δ_displacements(state)
-    # NOTE: May not warn if K is singular, see ONSAS#392.
-    cg!(ΔU, K, fₑₓₜ_red)
+	# Compute residual forces r = Fext
+	state.res_forces .= view(external_forces(state), free_dofs_idx)
+	linear_system.b = state.res_forces
 
-    # Update displacements into the state.
-    displacements(state)[free_dofs_idx] .= ΔU
+	# Update stiffness matrix K
+	if sa.current_step == 1
+		linear_system.A .= view(tangent_matrix(state), free_dofs_idx, free_dofs_idx)
+	end
+
+	# Compute ΔU
+	sol = solve!(linear_system)
+	ΔU = Δ_displacements!(state, sol.u)
+
+	# Update U
+	displacements(state)[free_dofs_idx] .= ΔU
 end
 
 end # module
--- src/StructuralAnalyses/StaticAnalyses.jl
+++ src/StructuralAnalyses/StaticAnalyses.jl
@@ -22,7 +22,7 @@ using ..StaticStates
 using ..Assemblers
 
 @reexport import ..StructuralAnalyses: initial_time, current_time, final_time, iteration_residuals,
-                                       is_done
+	is_done
 @reexport import ..StructuralSolvers: next!
 @reexport import ..Assemblers: assemble!, reset!
 
@@ -67,12 +67,12 @@ final_time(sa::AbstractStaticAnalysis) = last(load_factors(sa))
 
 "Return true if the structural analysis is completed."
 function is_done(sa::AbstractStaticAnalysis)
-    is_done_bool = if sa.current_step[] > length(load_factors(sa))
-        sa.current_step -= 1
-        true
-    else
-        false
-    end
+	is_done_bool = if sa.current_step[] > length(load_factors(sa))
+		sa.current_step -= 1
+		true
+	else
+		false
+	end
 end
 
 "Return the final load factor vector of an structural analysis."
@@ -87,67 +87,68 @@ next!(sa::AbstractStaticAnalysis) = sa.current_step += 1
 "Sets the current load factor of the structural analysis to the initial load factor.
 Also Reset! the iteration and `AbstractStructuralState`."
 function reset!(sa::AbstractStaticAnalysis)
-    sa.current_step = 1
-    reset!(current_state(sa))
-    @info "The current time of analysis have been reset."
-    sa
+	sa.current_step = 1
+	reset!(current_state(sa))
+	@info "The current time of analysis have been reset."
+	sa
 end
 
 "Assembles the Structure `s` (internal forces) during the `StaticAnalysis` `sa`."
 function assemble!(s::AbstractStructure, sa::AbstractStaticAnalysis)
-    state = current_state(sa)
+	state = current_state(sa)
 
-    # Reset assembled magnitudes
-    reset_assemble!(state)
+	# Reset assembled magnitudes
+	reset_assemble!(state)
 
-    for (mat, mat_elements) in pairs(materials(s))
-        for e in mat_elements
+	for (mat, mat_elements) in pairs(materials(s))
+		for e in mat_elements
 
-            # Global dofs of the element (dofs where K must be added)
-            u_e = view(displacements(state), local_dofs(e))
-            cache = elements_cache(state, e)
-            fᵢₙₜ_e, kₛ_e, σ_e, ϵ_e = internal_forces(mat, e, u_e, cache)
+			# Global dofs of the element (dofs where K must be added)
+			u_e = view(displacements(state), local_dofs(e))
+			cache = elements_cache(state, e)
+			fᵢₙₜ_e, kₛ_e, σ_e, ϵ_e = internal_forces(mat, e, u_e, cache)
 
-            # Assembles the element internal magnitudes
-            assemble!(state, fᵢₙₜ_e, e)
-            assemble!(state, kₛ_e, e)
-            assemble!(state, σ_e, ϵ_e, e)
-        end
-    end
+			# Assembles the element internal magnitudes
+			assemble!(state, fᵢₙₜ_e, e)
+			assemble!(state, kₛ_e, e)
+			assemble!(state, σ_e, ϵ_e, e)
+		end
+	end
 
-    # Insert values in the assembler objet into the sysyem tangent stiffness matrix
-    end_assemble!(state)
+	# Insert values in the assembler objet into the sysyem tangent stiffness matrix
+	end_assemble!(state)
 end
 
 "Reset the assembled magnitudes in the state."
 function reset_assemble!(state::StaticState)
-    reset!(assembler(state))
-    internal_forces(state) .= 0.0
-    K = tangent_matrix(state)
-    I, J, V = findnz(tangent_matrix(state))
-    K[I, J] .= zeros(eltype(V))
-    nothing
+	reset!(assembler(state))
+	internal_forces(state) .= 0.0
+	K = tangent_matrix(state)
+	I, J, V = findnz(tangent_matrix(state))
+	K[I, J] .= zeros(eltype(V))
+	nothing
 end
 
 "Push the current state into the solution."
 function Base.push!(st_sol::StatesSolution, c_state::StaticState)
-    # Copies TODO Need to store all these?
-    fdofs = free_dofs(c_state)
-    Uᵏ = deepcopy(displacements(c_state))
-    ΔUᵏ = deepcopy(Δ_displacements(c_state))
-    fₑₓₜᵏ = deepcopy(external_forces(c_state))
-    fᵢₙₜᵏ = deepcopy(internal_forces(c_state))
-    Kₛᵏ = deepcopy(tangent_matrix(c_state))
-    res_forces = deepcopy(c_state.res_forces)
-    σᵏ = dictionary([e => deepcopy(σ) for (e, σ) in pairs(stress(c_state))])
-    ϵᵏ = dictionary([e => deepcopy(ϵ) for (e, ϵ) in pairs(strain(c_state))])
-    iter_state = deepcopy(iteration_residuals(c_state))
-    # Empty assembler since the info is stored in k
-    assemblerᵏ = c_state.assembler
+	# Copies TODO Need to store all these?
+	fdofs = free_dofs(c_state)
+	Uᵏ = deepcopy(displacements(c_state))
+	ΔUᵏ = deepcopy(Δ_displacements(c_state))
+	fₑₓₜᵏ = deepcopy(external_forces(c_state))
+	fᵢₙₜᵏ = deepcopy(internal_forces(c_state))
+	Kₛᵏ = deepcopy(tangent_matrix(c_state))
+	res_forces = deepcopy(c_state.res_forces)
+	σᵏ = dictionary([e => deepcopy(σ) for (e, σ) in pairs(stress(c_state))])
+	ϵᵏ = dictionary([e => deepcopy(ϵ) for (e, ϵ) in pairs(strain(c_state))])
+	iter_state = deepcopy(iteration_residuals(c_state))
+	# Empty assembler since the info is stored in k
+	assemblerᵏ = c_state.assembler
+	linear_system = c_state.linear_system
 
-    state_copy = StaticState(fdofs, ΔUᵏ, Uᵏ, fₑₓₜᵏ, fᵢₙₜᵏ, Kₛᵏ, res_forces, ϵᵏ, σᵏ, assemblerᵏ,
-                             iter_state)
-    push!(states(st_sol), state_copy)
+	state_copy = StaticState(fdofs, ΔUᵏ, Uᵏ, fₑₓₜᵏ, fᵢₙₜᵏ, Kₛᵏ, res_forces, ϵᵏ, σᵏ, assemblerᵏ,
+		iter_state, linear_system)
+	push!(states(st_sol), state_copy)
 end
 
 end # module
